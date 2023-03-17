86945c5ad6d73df1436f7e03143faf18d9c92128
Russel L. Villacarlos <russelvillacarlos@gmail.com>
2023-03-17 10:39:16 +0800

    Remove the need for ArrayList

--- src/rlv/dcit25/algo/TowerOfHanoiSolver.java
+++ src/rlv/dcit25/algo/TowerOfHanoiSolver.java
@@ -1,7 +1,5 @@
 package rlv.dcit25.algo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,7 +20,7 @@ public class TowerOfHanoiSolver {
         in.nextLine();
         
         
-        List<Character> rods = new ArrayList(List.of('A','B','C'));
+        List<Character> rods = List.of('A','B','C');
         System.out.printf("Source rod (%s): ", rods.toString().replaceAll("\\[|\\]", ""));
         char source = in.next().toUpperCase().charAt(0);
 
